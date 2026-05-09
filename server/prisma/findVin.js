efcfb187ba44bff893d5882f9decc793666a93a2
2300032940 <230032940@kluniversity.in>
2026-05-09 22:20:13 +0530

    chore: remove sample data and internal scripts from repo

--- server/prisma/findVin.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const v = await prisma.vehicle.findUnique({ where: { vin: 'EH608226' } })
-  console.log('VEHICLE EH608226:', v)
-}
-main().catch(console.error).finally(() => prisma.$disconnect())
--- server/prisma/markData.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const result = await prisma.vehicle.updateMany({
-    data: { make: 'DELETED_BY_ANTIGRAVITY' }
-  })
-  console.log('Update result:', result)
-}
-main().catch(console.error).finally(() => prisma.$disconnect())
